16fb6c06c2497c6b47ab48814f18fb91dc95fae6
Pauliinaaa <89318549+Pauliinaaa@users.noreply.github.com>
2022-06-24 23:26:36 +0200

    Add files via upload

---
+++ src/Form/RecipeType.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Recipe type.
+ */
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Recipe;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class RecipeType.
+ */
+class RecipeType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @param array<string, mixed> $options
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add(
+            'name',
+            TextType::class,
+            [
+                'label' => 'label.name',
+                'required' => true,
+                'attr' => ['max_length' => 255],
+            ]
+        );
+        $builder->add(
+            'content',
+            TextType::class,
+            [
+                'label' => 'label_content',
+                'required' => true,
+                'attr' => ['max_length' => 1020],
+            ]
+        );
+        $builder->add(
+            'category',
+            EntityType::class,
+            [
+                'class' => Category::class,
+                'choice_label' => function ($category): string {
+                    return $category->getName();
+                },
+                'label' => 'label.category',
+                'placeholder' => 'label.none',
+                'required' => true,
+            ]
+        );
+    }
+
+    /**
+     * Configures the options for this type.
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => Recipe::class]);
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'recipe';
+    }
+}
---
+++ src/Service/UserService.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * User service.
+ */
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+/**
+ * Class UserService.
+ */
+class UserService
+{
+    /**
+     * User repository.
+     */
+    private UserRepository $userRepository;
+
+    /**
+     * UserPasswordHasherInterference.
+     */
+    private UserPasswordHasherInterface $passwordHasher;
+
+    /**
+     * UserService constructor.
+     */
+    public function __construct(UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->userRepository = $userRepository;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    /**
+     * Save user.
+     *
+     * @param User $user User entity
+     */
+    public function save(User $user): void
+    {
+        $this->userRepository->save($user);
+    }
+}
